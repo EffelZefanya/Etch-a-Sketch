4858060c14103ecc0eb83043f18e55680eb511e1
Fel <99955631+EffelZefanya@users.noreply.github.com>
2023-09-28 17:07:05 +0700

    feat:display size canvas is done

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-const canvas = document.querySelector('.canvas');
+const canvas = document.querySelector(".canvas");
 
-for (let i = 0; i < 16; i++) {
-  for(let j = 0; j < 16; j++){
-    const div = document.createElement('div');
-    div.style.width = `calc(100%/16)`;
-    div.style.height = `calc(100%/16)`;
-    canvas.appendChild(div);
+const size = document.querySelector("#sizeRange").value;
+
+const sizeDisplay = document.querySelector("#sizeDisplay");
+sizeDisplay.textContent = `Size of Canvas is: ${size}`;
+
+function drawCanvas(size) {
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      const div = document.createElement("div");
+      div.style.width = `calc(100%/${size})`;
+      div.style.height = `calc(100%/${size})`;
+      canvas.appendChild(div);
+    }
   }
 }
+
+drawCanvas(size);
