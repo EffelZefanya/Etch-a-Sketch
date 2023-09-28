56b2dfe87cd9d4c5e6adf0df3d700e9ed2668ea3
Fel <99955631+EffelZefanya@users.noreply.github.com>
2023-09-28 20:08:47 +0700

    feat:Black Coloring works

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let color = "black";
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (this.id === "reset") {
+      colorCanvasWhite();
+    } else {
+      color = this.id;
+    }
+  });
+});
+
 const canvas = document.querySelector(".canvas");
 
 const slider = document.querySelector("#sizeRange");
@@ -6,23 +19,48 @@ const sizeDisplay = document.querySelector("#sizeDisplay");
 
 let size = slider.value;
 sizeDisplay.textContent = `Size of Canvas is: ${size}`;
-canvas.style.gridTemplateColumns = "repeat(${size}, 1fr)";
-canvas.style.gridTemplateRows = "repeat(${size}, 1fr)";
+canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
 slider.oninput = () => {
+  removeDivInCanvas();
   size = slider.value;
   sizeDisplay.textContent = `Size of Canvas is: ${size}`;
-  canvas.style.gridTemplateColumns = "repeat(${size}, 1fr)";
-  canvas.style.gridTemplateRows = "repeat(${size}, 1fr)";
+  canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`;
   drawCanvas(size);
 };
 
+function removeDivInCanvas() {
+  const squaresInCanvas = canvas.querySelectorAll("div");
+  squaresInCanvas.forEach((div) => {
+    div.remove();
+  });
+}
+
+function coloring() {
+  if (color === "random") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 50%, 100%)`;
+  } else if (color === "eraser") {
+    this.style.backgroundColor = "white";
+  } else {
+    this.style.backgroundColor = color;
+  }
+}
+
+function colorCanvasWhite() {
+  const squaresInCanvas = canvas.querySelectorAll("div");
+  squaresInCanvas.forEach((div) => {
+    div.style.backgroundColor = "white";
+  });
+}
+
 function drawCanvas(size) {
-  for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      const div = document.createElement("div");
-      canvas.appendChild(div);
-    }
+  const totalSize = size * size;
+  for (let i = 0; i < totalSize; i++) {
+    const div = document.createElement("div");
+    div.addEventListener("mouseover", coloring);
+    canvas.appendChild(div);
   }
 }
 
