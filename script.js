729e9110cef15966efca27f9e737120bfb30bda6
Fel <99955631+EffelZefanya@users.noreply.github.com>
2023-09-28 16:40:17 +0700

    feat:canvas is drawn

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-const container = document.createElement("div");
-container.classList.add("container");
+const canvas = document.querySelector('.canvas');
 
+for (let i = 0; i < 16; i++) {
+  for(let j = 0; j < 16; j++){
+    const div = document.createElement('div');
+    div.style.width = `calc(100%/16)`;
+    div.style.height = `calc(100%/16)`;
+    canvas.appendChild(div);
+  }
+}
