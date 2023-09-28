1ed0bc507410bcee377c92a06e36234cc715ef6b
Fel <99955631+EffelZefanya@users.noreply.github.com>
2023-09-28 17:07:25 +0700

    feat:change to grid display

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 const canvas = document.querySelector(".canvas");
 
-const size = document.querySelector("#sizeRange").value;
+const slider = document.querySelector("#sizeRange");
 
 const sizeDisplay = document.querySelector("#sizeDisplay");
+
+let size = slider.value;
 sizeDisplay.textContent = `Size of Canvas is: ${size}`;
+canvas.style.gridTemplateColumns = "repeat(${size}, 1fr)";
+canvas.style.gridTemplateRows = "repeat(${size}, 1fr)";
+
+slider.oninput = () => {
+  size = slider.value;
+  sizeDisplay.textContent = `Size of Canvas is: ${size}`;
+  canvas.style.gridTemplateColumns = "repeat(${size}, 1fr)";
+  canvas.style.gridTemplateRows = "repeat(${size}, 1fr)";
+  drawCanvas(size);
+};
 
 function drawCanvas(size) {
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
       const div = document.createElement("div");
-      div.style.width = `calc(100%/${size})`;
-      div.style.height = `calc(100%/${size})`;
       canvas.appendChild(div);
     }
   }
